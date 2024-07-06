8b2c1efca8c09eb7626dfdc0395744971a6bb93c
ipv21 <willsherman771@gmail.com>
2024-07-06 07:44:15 -0400

    first commit

---
+++ src/components/News.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const News = () => {
+  return (
+    <div className='news-app'>
+      <div className='news-header'>
+        <hi className='logo'>News</hi>
+            
+      </div>
+    </div>
+  )
+}
+
+export default News
